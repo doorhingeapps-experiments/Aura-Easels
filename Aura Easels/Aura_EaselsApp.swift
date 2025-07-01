36426fb846c7e9e18aa0f0c0d75e2db7c4b131c2
Caedmon Myers <caedmonmyers@icloud.com>
2025-07-01 15:25:10 -0500

    Initial Commit

---
+++ Aura Easels/Aura_EaselsApp.swift
@@ -0,0 +1,20 @@
+//
+// Aura Easels
+// Aura_EaselsApp.swift
+//
+// Created on 7/1/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct Aura_EaselsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
